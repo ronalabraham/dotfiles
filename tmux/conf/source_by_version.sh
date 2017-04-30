c567ee3c1fdc630c3ac96505450dce1fce137dc5
Ronal Abraham <rabraham5@bloomberg.net>
2017-04-30 18:57:06 -0400

    support tmux 2.4+

--- tmux/conf/source_by_version.sh
+++ tmux/conf/source_by_version.sh
@@ -6,7 +6,9 @@
 tmux_conf=~/.tmux/conf
 tmux_version="$(tmux -V | cut -d ' ' -f 2 | sed 's/[^0-9\.]//g')"
 
-if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
+if [[ $(echo "$tmux_version >= 2.4" | bc) -eq 1 ]] ; then
+    tmux source-file "$tmux_conf/tmux_2.4.conf"
+elif [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
     tmux source-file "$tmux_conf/tmux_2.1.conf"
 elif [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
     tmux source-file "$tmux_conf/tmux_1.9.conf"
